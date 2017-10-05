e2e55743640a6f5e0012102a85717a931b2d853e
Estudiante <Estudiante@168.176.37.206>
2017-10-04 20:46:24 -0500

    04/10/17 15:56

---
+++ src/ejemplotanque/Tablero.java
@@ -0,0 +1,128 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplotanque;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+/**
+ *
+ * @author Estudiante
+ * Jpanel dado que voy a pintar en el Canvas
+ * ActionListener: Para poder ejecutar el escenario cada ciertos milisegundos
+ */
+public class Tablero extends JPanel implements ActionListener, MouseListener{
+    private Timer timer ;
+    private Tanque tanque;
+    
+    public Tablero(){
+        //Lanza un evento de tipo ActionListener cada 25 Milisegundo
+        //Se hace referencia a this porque la misma clase (Tablero) procesa el evento
+        this.timer = new Timer(25, this);
+        //Registrar evento del Teclado
+        setFocusable(true); //Debe estar en modo Focus para que puede detectar el evento
+        addKeyListener(new EventosTeclado()); //Inner class que procesa los eventos del teclado
+        addMouseListener (this);
+        this.timer.start(); //Inicio con el escenario
+        this.tanque = new Tanque (10,200);
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+//        Point mp = e.getPoint();
+//        Rectangle cuadrado = new Rectangle (this.xref, this.yref, 30, 30);
+//        if (cuadrado.contains(mp)){
+//            System.out.println("Click sobre el rectangulo");
+//        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+       
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+       
+    }
+        
+    //Inner class Que captura los eventos del teclado
+     private class EventosTeclado extends KeyAdapter {
+        //Cuando se suelta una tecla
+         @Override
+        public void keyReleased(KeyEvent e) {
+//           int key = e.getKeyCode();
+//           if (key == KeyEvent.VK_SPACE) {
+//            System.out.println("VK_SPACE"); //Se  va usar posteriormente 
+//           }
+        }
+        //Cuando se presiona una tecla
+        @Override
+            public void keyPressed(KeyEvent e) {
+//               int key = e.getKeyCode();
+//
+//               if (key == KeyEvent.VK_LEFT){
+//                   xref -=1;
+//               }
+//
+//               if (key == KeyEvent.VK_DOWN){
+//                   yref +=1;
+//               }
+//
+//               if (key == KeyEvent.VK_RIGHT){
+//                   xref +=1;
+//               }
+//
+//               if (key == KeyEvent.VK_UP){
+//                   yref -=1;
+//               }
+            }
+        }
+    
+    //Metodo donde se pintan los objetos 
+     @Override
+    public void paintComponent(Graphics g){
+       super.paintComponent(g);
+       Image imgTanque = loadImage(tanque.getImagenes()[tanque.getAngulo()]);
+       g.drawImage(imgTanque, tanque.getX(), tanque.getY(),this);
+
+    }
+
+    //Metodo que se ejecuta cada vez que se lanza un ActionListener
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("Repaint");
+        repaint();
+    }
+    
+    public Image loadImage (String imageName){
+        ImageIcon ii = new ImageIcon(imageName);
+        Image image = ii.getImage();
+        return image;
+    }
+}
---
+++ src/ejemplotanque/Tanque.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplotanque;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Tanque {
+    private int x;
+    private int y;
+    private int angulo;
+    private String[] imagenes;
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getAngulo() {
+        return angulo;
+    }
+
+    public String[] getImagenes() {
+        return imagenes;
+    }
+
+    public Tanque(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.angulo = 1;
+        this.imagenes = new String [18];
+        for (int i = 0; i< 18;i++){
+            this.imagenes[i] = (i+1) + ".png";
+        }
+    }
+}
